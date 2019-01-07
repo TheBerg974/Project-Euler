91d7832b35e027cb95b8a616682beb3dac259934
TheBerg974 <32581690+TheBerg974@users.noreply.github.com>
2019-01-07 17:58:12 -0500

    number three

---
+++ ProjectEuler/src/projecteuler/numberThree.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecteuler;
+
+/**
+ *
+ * @author Sam
+ */
+public class numberThree {
+    
+    /*The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143 ?*/
+    
+    public static void main(String[] args) {
+        long num = 600851475143L;
+        long counter = 0;
+        
+        for (double i = Math.floor(Math.sqrt(num)); i > 0; i--) {
+            double temp = num/i;
+            if(temp == (int)temp) {
+                long number = (long)i;
+                if(isPrime(number)) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+        
+        
+    }
+    
+    public static boolean isPrime(long num) {
+        if (num % 2 == 0) {
+          return false; // checks divisibility by 2
+        }
+        for (long i=3; i*i<=num; i+=2) {
+          if (num % i == 0) {
+            return false;
+          }
+        }
+        return true;
+   }
+}
